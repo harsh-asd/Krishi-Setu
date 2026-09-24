8b3557de29de7315b49c96f80b60d8bcf9e399eb
Harsh Singh <singh.harsh.asd@gmail.com>
2026-09-24 14:05:44 +0530

    fix(transporter): add missing Logo import to fix login page crash

--- src/pages/transporter/TransporterLogin.jsx
+++ src/pages/transporter/TransporterLogin.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useMemo, useState } from "react";
 import { useNavigate } from "react-router";
+import Logo from "../../components/Logo";
 import tractorHarvest from "../../assets/backgrounds/tractor-harvest.jpg";
 import goldenOxPlowing from "../../assets/backgrounds/golden-ox-plowing.png";
 import {
